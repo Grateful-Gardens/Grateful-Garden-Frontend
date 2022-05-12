e03716f214c375e14c68a03d8b5add2a6a54e105
Jah821 <91166820+Jah821@users.noreply.github.com>
2022-05-12 17:57:16 -0400

    started mapbox rendering

---
+++ mapbox/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MapBox from './components/MapBox';
+// import mapboxgl from '!mapbox-gl';
+
+function App() {
+  return (
+    <div className="App">
+      <MapBox />
+    </div>
+  );
+}
+
+export default App;
