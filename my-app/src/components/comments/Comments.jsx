02288a8b3c0f54baecfa126e3f85d755795e80bb
Zohaib <zohaibmanzoor1700@gmail.com>
2022-05-21 17:26:44 -0700

    delete comments working

--- my-app/src/components/comments/Comments.jsx
+++ my-app/src/components/comments/Comments.jsx
@@ -8,12 +8,28 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
+import DeleteIcon from "@mui/icons-material/Delete";
 
+export default function Comments({allComments, aComment, setComments }) {
 
-export default function Comments({allComments}) {
+  const handleDelete = async (e) => {
+    try {
+      await fetch(`http://localhost:9001/comments/${allComments.comment_id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      const filtered = aComment.filter((c) => c.comment_id != allComments.comment_id)
+      setComments(filtered)
+    } catch(error) {
+      console.log(error)
+    }
+  } 
+
   return (
     <div>
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <List sx={{ width: '100%', maxWidth: 360}}>
       <ListItem alignItems="flex-start">
         <ListItemAvatar>
           <Avatar alt="" src="/static/images/avatar/1.jpg" />
@@ -31,6 +47,7 @@ export default function Comments({allComments}) {
               </Typography>
               {allComments.username} {" "}
               {DateTime.fromISO(allComments.time_posted).toRelative()}
+              <DeleteIcon className='delete-comment' type="submit" onClick={handleDelete} />
             </React.Fragment>
           }
         />
